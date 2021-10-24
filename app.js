203307ffee31935cac539f5e6ce10bdff9497561
Pope17 <ayrtonjuarez97@gmail.com>
2021-10-24 18:03:18 -0300

    Keroku

--- app.js
+++ app.js
@@ -5,9 +5,13 @@ const PUERTO = 0911;
 
 app.use(express.static('./public'));
 
-app.listen(PUERTO, function(){
+/*app.listen(PUERTO, function(){
     console.log('Servidor corriendo en la ruta 0911')
-});
+});*/
+
+app.listen(process.env.PORT || PUERTO, function(){
+    console.log('Servidor corriendo en la ruta 0911')
+})
 
 app.get('/', function(req,res){
     res.sendFile(path.join(__dirname, './index.html'))
